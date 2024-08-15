e64cfc14a315c5290ae461b75f18dd2c9d5efcb8
blake-butsko <89292796+blake-butsko@users.noreply.github.com>
2024-08-15 18:03:42 -0400

    test commit

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
+// This is a whole new thing I'm wroking on right here
+
 // Create a scene, camera, and renderer
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
